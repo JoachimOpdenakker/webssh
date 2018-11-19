6f8fa01eda450cd6c0ceddef10f44686766cd736
Joachim Opdenakker <joachim.opdenakker@student.ucll.be>
2018-11-19 01:27:40 +0100

    changed to venv

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-var=$(pwd)
+script_name=$0
+script_path=$(dirname "$0")
+$script_path/generateservicefile.sh
+python3 -m venv websshvenv
+source websshvenv/bin/activate
 python3 setup.py install --record files.txt
-cp "$var/webssh.service" "/lib/systemd/system/"
+cp "$script_path/webssh.service" "/lib/systemd/system/"
 systemctl daemon-reload
